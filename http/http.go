b3131f8654630b0fafa4c02308ffef20a2108d9e
Alberto Mancini <alberto@k-teq.com>
2025-03-15 09:28:35 +0100

    adds raw-inline api

--- http/http.go
+++ http/http.go
@@ -82,7 +82,9 @@ func NewHandler(
 	api.Handle("/settings", monkey(settingsGetHandler, "")).Methods("GET")
 	api.Handle("/settings", monkey(settingsPutHandler, "")).Methods("PUT")
 
+	api.PathPrefix("/raw-inline").Handler(monkey(rawInlineHandler, "/api/raw-inline")).Methods("GET")
 	api.PathPrefix("/raw").Handler(monkey(rawHandler, "/api/raw")).Methods("GET")
+
 	api.PathPrefix("/preview/{size}/{path:.*}").
 		Handler(monkey(previewHandler(imgSvc, fileCache, server.EnableThumbnails, server.ResizePreview), "/api/preview")).Methods("GET")
 	api.PathPrefix("/command").Handler(monkey(commandsHandler, "/api/command")).Methods("GET")
