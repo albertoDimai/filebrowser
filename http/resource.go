057ae8c5a6fd05a20a737f7431e9fe3666fce014
Alberto Mancini <alberto@k-teq.com>
2025-04-12 11:05:13 +0200

    fix non-wait for pdflatex replacing cmd.Start() with cmd.Run()

--- http/resource.go
+++ http/resource.go
@@ -336,6 +336,7 @@ func patchAction(ctx context.Context, action, src, dst string, request  *http.Re
 		}
 
 		return fileutils.MoveFile(d.user.Fs, src, dst)
+
 	case "unzip":
 		if !d.user.Perm.Unzip {
 			return fbErrors.ErrPermissionDenied
@@ -350,38 +351,31 @@ func patchAction(ctx context.Context, action, src, dst string, request  *http.Re
 		}
 		src = d.user.FullPath(src)
 		dst = d.user.FullPath(dst)
-
 		println("executing: " + "pdflatex.wrapper.sh " + src  + " " + dst  )
-
 		cmd := exec.Command("pdflatex.wrapper.sh", src, dst) //nolint:gosec
-		return cmd.Start()
+		return cmd.Run()
 
 	case "mauro:m2hv":
 		if false /*|| !d.user.Perm.Mauro*/ {
 			return fbErrors.ErrPermissionDenied
 		}
-
 		src = d.user.FullPath(src)
 		dst = d.user.FullPath(dst)
-
 		comamndline_arguments := request.URL.Query().Get("commandline");
 		comamndline, err := url.QueryUnescape(comamndline_arguments);
 		if err != nil {
 			return fmt.Errorf("error parsing options %s: %w", comamndline_arguments, fbErrors.ErrInvalidRequestParams)
 		}
-		//
 		println("executing: " + "m2hv.wrapper.sh " + src  + " " + dst + " " +  comamndline )
-
 		cmd := exec.Command("m2hv.wrapper.sh", src, dst, comamndline) //nolint:gosec
 		return cmd.Run()
+
 	case "mauro:m2lv":
 		if false /*|| !d.user.Perm.Mauro*/ {
 			return fbErrors.ErrPermissionDenied
 		}
-
 		src = d.user.FullPath(src)
 		dst = d.user.FullPath(dst)
-
 		comamndline_arguments := request.URL.Query().Get("commandline");
 		comamndline, err := url.QueryUnescape(comamndline_arguments);
 		if err != nil {
@@ -392,6 +386,7 @@ func patchAction(ctx context.Context, action, src, dst string, request  *http.Re
 
 		cmd := exec.Command("m2lv.wrapper.sh", src, dst, comamndline) //nolint:gosec
 		return cmd.Run()
+
 	case "mauro:m2ledmac":
 		if false /*|| !d.user.Perm.Mauro*/ {
 			return fbErrors.ErrPermissionDenied
@@ -408,6 +403,7 @@ func patchAction(ctx context.Context, action, src, dst string, request  *http.Re
 
 		cmd := exec.Command("m2ledmac.wrapper.sh", src, dst, comamndline) //nolint:gosec
 		return cmd.Run()
+
 	default:
 		return fmt.Errorf("unsupported action %s: %w", action, fbErrors.ErrInvalidRequestParams)
 	}
